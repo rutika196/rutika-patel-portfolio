eac8616bccaf7718b8bb2d98b57dc71b7db36db4
Rutika Patel <rutikap0619@gmail.com>
2025-04-17 15:49:26 -0400

    Add Medium icon to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import logo from "../assets/Rp_logo_gray600.png";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
+import { FaMedium } from "react-icons/fa";
 
 export const Navbar = () => {
   return (
@@ -9,6 +10,13 @@ export const Navbar = () => {
         <img className="mx-2 w-10" src={logo} alt="logo" />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a
+          href="https://medium.com/@rutikap0619/%EF%B8%8F-welcome-to-my-coding-castle-rutika-and-the-goblet-of-frontend-flame-9e1ff05f9c8a"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaMedium className="hover:text-black transition duration-300" />
+        </a>
         <a
           href="https://www.linkedin.com/in/rutika-patel"
           target="_blank"
